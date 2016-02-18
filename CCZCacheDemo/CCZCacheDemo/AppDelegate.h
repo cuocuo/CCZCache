0a010cc8985e6db130d0ddd96141c4c2dd7b4a6f
shiheng <shiheng1413@163.com>
2016-02-19 01:13:32 +0800

    init project

---
+++ CCZCacheDemo/CCZCacheDemo/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  CCZCacheDemo
+//
+//  Created by 郑雨阳 on 16/2/19.
+//  Copyright © 2016年 cuocuo. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ CCZCacheDemo/CCZCacheDemo/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  CCZCacheDemo
+//
+//  Created by 郑雨阳 on 16/2/19.
+//  Copyright © 2016年 cuocuo. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
